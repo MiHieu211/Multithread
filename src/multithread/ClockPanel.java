03376bf39316ea5d4847648769e82a5f8284497e
John Doe <hieunm211@gmail.com>
2024-05-16 22:46:45 +0700

    new add

---
+++ src/multithread/ClockPanel.java
@@ -0,0 +1,16 @@
+package multithread;
+
+import javax.swing.JPanel;
+
+public class ClockPanel extends JPanel {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Create the panel.
+	 */
+	public ClockPanel() {
+
+	}
+
+}
